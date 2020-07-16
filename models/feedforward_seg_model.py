24648ff38d277219e2d89f5cec78fe5880012242
Quentin Uhl <quentin.uhl@epfl.ch>
2020-07-16 04:27:09 +0200

    Update feedforward_seg_model.py

--- models/feedforward_seg_model.py
+++ models/feedforward_seg_model.py
@@ -139,12 +139,14 @@ class FeedForwardSegmentation(BaseModel):
         self.loss_S = self.criterion(self.prediction, self.target)
 
     def get_segmentation_stats(self):
-        self.seg_scores, self.class_dice_score, self.overall_dice_score, self.roc_auc_score, self.WBCE_score, self.L1_score, self.Volume_score = segmentation_stats(self.prediction, self.target, self.output_nc, self.output_cdim)
+        self.seg_scores, self.class_dice_score, self.chan_dice_score, self.overall_dice_score, self.roc_auc_score, self.WBCE_score, self.L1_score, self.Volume_score = segmentation_stats(self.prediction, self.target, self.output_nc, self.output_cdim)
         seg_stats = [('Overall_Acc', self.seg_scores['overall_acc']), ('Mean_IOU', self.seg_scores['mean_iou']),
                      ('Overall_Dice', self.overall_dice_score), ('ROC_AUC', self.roc_auc_score),
                      ('WBCE_score', self.WBCE_score), ('L1_score', self.L1_score), ('Volume_score', self.Volume_score)]
         for class_id in range(self.class_dice_score.size):
             seg_stats.append(('Class_{}'.format(class_id), self.class_dice_score[class_id]))
+        for chan_id in range(self.chan_dice_score.size):
+            seg_stats.append(('Channel_{}'.format(chan_id), self.chan_dice_score[chan_id]))
         return OrderedDict(seg_stats)
 
     def get_current_errors(self):
