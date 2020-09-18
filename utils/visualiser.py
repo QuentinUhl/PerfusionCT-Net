2de1e42783295ae99cdfd38c0f547a1ed5e0ecf7
Quentin Uhl <quentin.uhl@epfl.ch>
2020-09-18 16:34:27 +0200

    Update visualiser.py

--- utils/visualiser.py
+++ utils/visualiser.py
@@ -169,20 +169,30 @@ class Visualiser():
 
     def plot_line(self, x, y, key, split_name):
         if split_name == 'test':
-            self.error_wins[key] = self.display_id * 3 + len(self.error_wins)
-            self.error_plots[key] = self.vis.line(
-                X=np.array([x, x]),
-                Y=np.array([y, y]),
-                opts=dict(
-                    legend=[split_name],
-                    title=self.name + ' {} over time'.format(key),
-                    xlabel='Epochs',
-                    ylabel=key,
-                    markers=True,
-                    markersymbol='+',
-                    markersize = 7,
-                    win=self.error_wins[key]
-            ))
+            if key not in self.error_plots:
+                self.error_wins[key] = self.display_id * 3 + len(self.error_wins)
+                self.error_plots[key] = self.vis.line(
+                    X=np.array([x, x]),
+                    Y=np.array([y, y]),
+                    opts=dict(
+                        legend=[split_name],
+                        title=self.name + ' {} over time'.format(key),
+                        xlabel='Epochs',
+                        ylabel=key,
+                        markers=True,
+                        markersymbol='+',
+                        markersize = 7,
+                        win=self.error_wins[key]
+                ))
+            else:
+                self.vis.line(X=np.array([x]), 
+                              Y=np.array([y]), 
+                              markers=True, 
+                              markersymbol='+', 
+                              markersize = 7,
+                              win=self.error_plots[key], 
+                              name=split_name, 
+                              update='append')
         else:
             if key not in self.error_plots:
                 self.error_wins[key] = self.display_id * 3 + len(self.error_wins)
