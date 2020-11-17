083e28651bacc4dcc22d43a98df7912c861d67cb
Quentin Uhl <quentin.uhl@epfl.ch>
2020-11-17 23:32:42 +0100

    Correct filters

--- models/networks/unet_pCT_cd_multi_down_3D.py
+++ models/networks/unet_pCT_cd_multi_down_3D.py
@@ -16,64 +16,64 @@ class unet_pCT_cd_multi_down_3D(nn.Module):
         self.is_batchnorm = is_batchnorm
         self.feature_scale = feature_scale
 
-        filters = [64, 128, 256, 512, 1024]
-        filters = [int(x / self.feature_scale) for x in filters]
+        self.filters = [64, 128, 256, 512, 1024]
+        self.filters = [int(x / self.feature_scale) for x in self.filters]
 
         # downsampling
-        self.conv1 = UnetConv3(self.in_channels, filters[0], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
+        self.conv1 = UnetConv3(self.in_channels, self.filters[0], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
         self.maxpool1 = nn.MaxPool3d(kernel_size=(2, 2, 2))
 
-        self.conv2 = UnetConv3(filters[0], filters[1], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
+        self.conv2 = UnetConv3(self.filters[0], self.filters[1], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
         self.maxpool2 = nn.MaxPool3d(kernel_size=(2, 2, 2))
 
-        self.conv3 = UnetConv3(filters[1], filters[2], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
+        self.conv3 = UnetConv3(self.filters[1], self.filters[2], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
         self.maxpool3 = nn.MaxPool3d(kernel_size=(2, 2, 2))
 
-        self.conv4 = UnetConv3(filters[2], filters[3], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
+        self.conv4 = UnetConv3(self.filters[2], self.filters[3], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
         self.maxpool4 = nn.MaxPool3d(kernel_size=(2, 2, 2))
 
-        self.center = UnetConv3(filters[3], filters[4], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
-        self.gating = UnetGridGatingSignal3(filters[4], filters[4], kernel_size=(1, 1, 1), is_batchnorm=self.is_batchnorm)
+        self.center = UnetConv3(self.filters[3], self.filters[4], self.is_batchnorm, kernel_size=(3,3,3), padding_size=(1,1,1))
+        self.gating = UnetGridGatingSignal3(self.filters[4], self.filters[4], kernel_size=(1, 1, 1), is_batchnorm=self.is_batchnorm)
         
         # add clinical data
-        self.fc1 = nn.Linear(21,filters[0])
-        self.attentionblockmed1 = MultiAttentionBlock(in_size=filters[4], gate_size=filters[4], inter_size=filters[1],
+        self.fc1 = nn.Linear(21,self.filters[0])
+        self.attentionblockmed1 = MultiAttentionBlock(in_size=self.filters[4], gate_size=self.filters[4], inter_size=self.filters[1],
                                                      nonlocal_mode=nonlocal_mode, sub_sample_factor=attention_dsample)
-        self.fc2 = nn.Linear(21,filters[1])
-        self.attentionblockmed2 = MultiAttentionBlock(in_size=filters[4], gate_size=filters[4], inter_size=filters[1],
+        self.fc2 = nn.Linear(21,self.filters[1])
+        self.attentionblockmed2 = MultiAttentionBlock(in_size=self.filters[4], gate_size=self.filters[4], inter_size=self.filters[1],
                                                      nonlocal_mode=nonlocal_mode, sub_sample_factor=attention_dsample)
-        self.fc3 = nn.Linear(21,filters[2])
-        self.attentionblockmed3 = MultiAttentionBlock(in_size=filters[4], gate_size=filters[4], inter_size=filters[1],
+        self.fc3 = nn.Linear(21,self.filters[2])
+        self.attentionblockmed3 = MultiAttentionBlock(in_size=self.filters[4], gate_size=self.filters[4], inter_size=self.filters[1],
                                                      nonlocal_mode=nonlocal_mode, sub_sample_factor=attention_dsample)
-        self.fc4 = nn.Linear(21,filters[3])
-        self.attentionblockmed4 = MultiAttentionBlock(in_size=filters[4], gate_size=filters[4], inter_size=filters[1],
+        self.fc4 = nn.Linear(21,self.filters[3])
+        self.attentionblockmed4 = MultiAttentionBlock(in_size=self.filters[4], gate_size=self.filters[4], inter_size=self.filters[1],
                                                      nonlocal_mode=nonlocal_mode, sub_sample_factor=attention_dsample)
 
         self.fc5a = nn.Linear(21, 64)
-        self.fc5b = nn.Linear(64,filters[4]) #256*1*1*1
+        self.fc5b = nn.Linear(64,self.filters[4]) #256*1*1*1
         self.relu = nn.ReLU()
-        self.attentionblockmed5 = MultiAttentionBlock(in_size=filters[4], gate_size=filters[4], inter_size=filters[1],
+        self.attentionblockmed5 = MultiAttentionBlock(in_size=self.filters[4], gate_size=self.filters[4], inter_size=self.filters[1],
                                                    nonlocal_mode=nonlocal_mode, sub_sample_factor= attention_dsample)
 
         # attention blocks
-        self.attentionblock2 = MultiAttentionBlock(in_size=filters[1], gate_size=filters[2], inter_size=filters[1],
+        self.attentionblock2 = MultiAttentionBlock(in_size=self.filters[1], gate_size=self.filters[2], inter_size=self.filters[1],
                                                    nonlocal_mode=nonlocal_mode, sub_sample_factor= attention_dsample)
-        self.attentionblock3 = MultiAttentionBlock(in_size=filters[2], gate_size=filters[3], inter_size=filters[2],
+        self.attentionblock3 = MultiAttentionBlock(in_size=self.filters[2], gate_size=self.filters[3], inter_size=self.filters[2],
                                                    nonlocal_mode=nonlocal_mode, sub_sample_factor= attention_dsample)
-        self.attentionblock4 = MultiAttentionBlock(in_size=filters[3], gate_size=filters[4], inter_size=filters[3],
+        self.attentionblock4 = MultiAttentionBlock(in_size=self.filters[3], gate_size=self.filters[4], inter_size=self.filters[3],
                                                    nonlocal_mode=nonlocal_mode, sub_sample_factor= attention_dsample)
 
         # upsampling
-        self.up_concat4 = UnetUp3_CT(filters[4], filters[3], is_batchnorm)
-        self.up_concat3 = UnetUp3_CT(filters[3], filters[2], is_batchnorm)
-        self.up_concat2 = UnetUp3_CT(filters[2], filters[1], is_batchnorm)
-        self.up_concat1 = UnetUp3_CT(filters[1], filters[0], is_batchnorm)
+        self.up_concat4 = UnetUp3_CT(self.filters[4], self.filters[3], is_batchnorm)
+        self.up_concat3 = UnetUp3_CT(self.filters[3], self.filters[2], is_batchnorm)
+        self.up_concat2 = UnetUp3_CT(self.filters[2], self.filters[1], is_batchnorm)
+        self.up_concat1 = UnetUp3_CT(self.filters[1], self.filters[0], is_batchnorm)
 
         # deep supervision
-        self.dsv4 = UnetDsv3(in_size=filters[3], out_size=n_classes, scale_factor=8)
-        self.dsv3 = UnetDsv3(in_size=filters[2], out_size=n_classes, scale_factor=4)
-        self.dsv2 = UnetDsv3(in_size=filters[1], out_size=n_classes, scale_factor=2)
-        self.dsv1 = nn.Conv3d(in_channels=filters[0], out_channels=n_classes, kernel_size=1)
+        self.dsv4 = UnetDsv3(in_size=self.filters[3], out_size=n_classes, scale_factor=8)
+        self.dsv3 = UnetDsv3(in_size=self.filters[2], out_size=n_classes, scale_factor=4)
+        self.dsv2 = UnetDsv3(in_size=self.filters[1], out_size=n_classes, scale_factor=2)
+        self.dsv1 = nn.Conv3d(in_channels=self.filters[0], out_channels=n_classes, kernel_size=1)
 
         # final conv (without any concat)
         self.final = nn.Conv3d(n_classes*4, n_classes, 1)
@@ -92,28 +92,28 @@ class unet_pCT_cd_multi_down_3D(nn.Module):
         conv1 = self.conv1(inputs)
         print("conv1 size : ", conv1.shape)
         print("cd_fc1 size : ", cd_fc1.shape)
-        cd_fc1 = cd_fc1.view((-1, filters[1], 1, 1, 1)) * conv1
+        cd_fc1 = cd_fc1.view((-1, self.filters[1], 1, 1, 1)) * conv1
         conv1, _ = self.attentionblockmed1(conv1, cd_fc1)
         maxpool1 = self.maxpool1(conv1)
 
         cd_fc2 = self.fc2(clinical_data.float())
         cd_fc2 = self.relu(cd_fc2)
         conv2 = self.conv2(maxpool1)
-        cd_fc2 = cd_fc2.view((-1, filters[2], 1, 1, 1)) * conv2
+        cd_fc2 = cd_fc2.view((-1, self.filters[2], 1, 1, 1)) * conv2
         conv2, _ = self.attentionblockmed2(conv2, cd_fc2)
         maxpool2 = self.maxpool2(conv2)
 
         cd_fc3 = self.fc3(clinical_data.float())
         cd_fc3 = self.relu(cd_fc3)
         conv3 = self.conv3(maxpool2)
-        cd_fc3 = cd_fc3.view((-1, filters[3], 1, 1, 1)) * conv3
+        cd_fc3 = cd_fc3.view((-1, self.filters[3], 1, 1, 1)) * conv3
         conv3, _ = self.attentionblockmed3(conv3, cd_fc3)
         maxpool3 = self.maxpool3(conv3)
 
         cd_fc4 = self.fc4(clinical_data.float())
         cd_fc4 = self.relu(cd_fc4)
         conv4 = self.conv4(maxpool3)
-        cd_fc4 = cd_fc4.view((-1, filters[4], 1, 1, 1)) * conv4
+        cd_fc4 = cd_fc4.view((-1, self.filters[4], 1, 1, 1)) * conv4
         conv4, _ = self.attentionblockmed4(conv4, cd_fc4)
         maxpool4 = self.maxpool4(conv4)
 
@@ -127,7 +127,7 @@ class unet_pCT_cd_multi_down_3D(nn.Module):
         cd_fc5a = self.relu(cd_fc5a)
         cd_fc5b = self.fc5b(cd_fc5a)
         cd_fc5b = self.relu(cd_fc5b)
-        decoded_clinical_data = cd_fc5b.view((-1, filters[4], 1, 1, 1)) * center
+        decoded_clinical_data = cd_fc5b.view((-1, self.filters[4], 1, 1, 1)) * center
         # print("decoded clinical size : ", decoded_clinical_data.shape)
         pregating, _ = self.attentionblockmed5(center, decoded_clinical_data)
         
