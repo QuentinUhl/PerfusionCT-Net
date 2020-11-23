ca36ca9d1b1a64cc2c597723159610eb3447b1a1
Quentin Uhl <quentin.uhl@epfl.ch>
2020-11-23 19:26:12 +0100

    Update __init__.py

--- models/networks/__init__.py
+++ models/networks/__init__.py
@@ -55,6 +55,6 @@ def _get_model_instance(name, tensor_dim):
         'unet_pCT_cd_multi_down': {'3D': unet_pCT_cd_multi_down_3D},
         'unet_with_cd': {'3D': unet_with_cd_3D},
         'unet_without_cd': {'3D': unet_without_cd_3D},
-        'kiunet': {'3D': KiUnet_3D}
+        'kiunet': {'3D': kiunet_3D}
         
     }[name][tensor_dim]
